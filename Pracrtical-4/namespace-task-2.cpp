c1b9052a798c1f32ec2473e142003e2551e633ca
knockcat <85362504+knockcat@users.noreply.github.com>
2021-09-29 09:53:47 +0530

    Create namespace-task-2.cpp
    Namesapce

---
+++ Pracrtical-4/namespace-task-2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+namespace first
+{
+	void saysomething()
+	{
+		cout<<"1.. first namespace	Hello world\n";
+	}
+}
+
+using namespace first;
+
+namespace second
+{
+	void saysomething()
+	{
+		cout<<"2.. seccond namespace	Hello world\n";
+	}
+}
+
+
+int main()
+{
+	//first::saysomething();
+	saysomething();
+	second::saysomething();
+
+	return 0;
+}
